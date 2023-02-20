862a3d62705237ce444430c8246849515469dce4
Negin <negin.khademi@gmail.com>
2023-02-20 13:32:32 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { user } from './app-interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nameProject';
+
+  users :user[] = [
+    {
+      email:'a@a.com',
+      password:'123',
+      address:'asdf'
+    },
+    {
+      email:'b@b.com',
+      password:'456',
+      address:'qwqr'
+    },
+    {
+      email:'c@c.com',
+      password:'789',
+      address:'zcvcv'
+    }
+  ]
+
+  public addNewUser(userEmail:string,userPassword:string,userAddress:string,e:Event){
+    e.preventDefault();
+    let u:user ={
+      email: userEmail,
+      password : userPassword,
+      address : userAddress
+    };
+    this.users.push(u)
+  }
+
+
 }
+
